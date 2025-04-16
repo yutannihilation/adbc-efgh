8ddd1470aaec4be3af63bbb2b156c06f49d1a01e
Hiroaki Yutani <yutani.ini@gmail.com>
2025-04-17 00:30:35 +0900

    Fix HTTP/2 server

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use hyper::{server::conn::http2, service::service_fn};
 use hyper_util::rt::{TokioExecutor, TokioIo};
 use rustls::pki_types::{CertificateDer, PrivateKeyDer};
 use tokio::{io::AsyncReadExt, net::TcpListener};
+use tokio_rustls::TlsAcceptor;
+
 use tracing::{error, info, trace_span};
 
 use h3::server::RequestResolver;
@@ -16,9 +18,7 @@ use http_body_util::Full;
 
 const SERVER_CERT: &[u8] = include_bytes!("cert/server.cert");
 const SERVER_KEY: &[u8] = include_bytes!("cert/server.key");
-const ADDRESS: &str = "[::1]:4433";
-
-static ALPN: &[u8] = b"h3";
+const ADDRESS: &str = "[::1]:443";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -29,25 +29,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_max_level(tracing::Level::INFO)
         .init();
 
-    // create quinn server endpoint and bind UDP socket
-
-    // both cert and key must be DER-encoded
     let cert = CertificateDer::from(SERVER_CERT);
     let key = PrivateKeyDer::try_from(SERVER_KEY)?;
 
-    let mut tls_config = rustls::ServerConfig::builder()
+    let mut tls_config_http3 = rustls::ServerConfig::builder()
         .with_no_client_auth()
         .with_single_cert(vec![cert], key)?;
+    tls_config_http3.max_early_data_size = u32::MAX;
 
-    tls_config.max_early_data_size = u32::MAX;
-    tls_config.alpn_protocols = vec![ALPN.into()];
+    let mut tls_config_http2 = tls_config_http3.clone();
+
+    tls_config_http3.alpn_protocols = vec![b"h3".into()];
+    let server_config_http3 =
+        quinn::ServerConfig::with_crypto(Arc::new(QuicServerConfig::try_from(tls_config_http3)?));
+
+    tls_config_http2.alpn_protocols = vec![b"h2".into()];
+    let tls_acceptor = TlsAcceptor::from(Arc::new(tls_config_http2));
 
     let address = ADDRESS.parse()?;
 
-    let server_config =
-        quinn::ServerConfig::with_crypto(Arc::new(QuicServerConfig::try_from(tls_config)?));
-    let endpoint_http3 = quinn::Endpoint::server(server_config, address)?;
-
+    let endpoint_http3 = quinn::Endpoint::server(server_config_http3, address)?;
     let endpoint_http2 = TcpListener::bind(address).await?;
 
     info!("listening on {ADDRESS}");
@@ -57,12 +58,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         tokio::select! {
             Ok((stream, _)) = endpoint_http2.accept() => {
+
+                let tls_acceptor = tls_acceptor.clone();
+
                 trace_span!("New HTTP/2 connection being attempted");
 
-                let io = TokioIo::new(stream);
 
                 // handle_http2_request
                 tokio::task::spawn(async move {
+                    let io = match tls_acceptor.accept(stream).await {
+                        Ok(tls_stream) => TokioIo::new(tls_stream),
+                        Err(err) => {
+                            eprintln!("failed to perform tls handshake: {err:#}");
+                            return;
+                        }
+                    };
+
                     // Handle the connection from the client using HTTP/2 with an executor and pass any
                     // HTTP requests received on that connection to the `hello` function
                     if let Err(err) = http2::Builder::new(TokioExecutor::new())
